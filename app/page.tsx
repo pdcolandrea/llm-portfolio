878d992f67d6ff254657fa38af7854ded120a692
Paul <colandreapaul@gmail.com>
2023-05-01 16:54:02 -0400

    add home layout

--- app/page.tsx
+++ app/page.tsx
@@ -24,23 +24,31 @@ export default async function Home() {
 
   return (
     <>
-      <div className="z-10 h-screen w-full max-w-xl px-5 xl:px-0">
+      <div className="z-10 w-full max-w-xl px-5 xl:px-0">
         <div className="mx-auto mb-4 h-28 w-28 rounded-full bg-red-200" />
         <h1
           className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm md:text-7xl md:leading-[5rem]"
           style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
         >
-          <Balancer>Hi! Im Paul.</Balancer>
+          <Balancer>Hi! My name is Paul.</Balancer>
         </h1>
         <p
-          className="mt-6 animate-fade-up text-center text-gray-500 opacity-0 md:text-xl"
+          className="mb-[430px] mt-6 animate-fade-up text-center text-gray-500 opacity-0 md:text-xl"
           style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
         >
           <Balancer>
             Digital crafter specializing in Frontend development.
           </Balancer>
         </p>
+
+      <div className="items-center justify-center text-center">
+      <p className="text-gray-700">
+          Tampa, FL 🇺🇲 
+        </p>
+      <small className="text-gray-700">&#123;4:30PM&#125; </small>
+        </div>
       </div>
+
       <div className="my-10 grid w-full max-w-screen-xl animate-fade-up grid-cols-1 gap-5 px-5 md:grid-cols-3 xl:px-0">
         {features.map(({ title, description, demo, large }) => (
           <Card
